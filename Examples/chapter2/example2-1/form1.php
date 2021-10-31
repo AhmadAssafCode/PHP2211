65131cc61b10d0100d595b0b90c61a12d02ce8c3
Ahmad Assaf <ahmadassaf@paluniv.edu.ps>
2021-11-01 00:43:15 +0200

    example 2-1

---
+++ Examples/chapter2/example2-1/form1.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>Form 1</title>
+</head>
+
+<body>
+    <h2> Example 2-1 Form1</h2>
+    <form action="welcome.php" method="post">
+        Name: <input type="text" name="name"><br><br>
+        Email: <input type="text" name="email"><br><br>
+        <input type="submit">
+    </form>
+</body>
+
+</html>
---
+++ Examples/chapter2/example2-1/welcome.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Welcome</title>
+</head>
+
+<body>
+    <h2> Example 2-1 Welcome</h2>
+    Welcome <?php echo $_POST["name"]; ?><br />
+    Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+
+</html>
