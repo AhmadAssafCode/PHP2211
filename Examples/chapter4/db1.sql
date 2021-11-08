9308a82e09f076bbecf7b251b174652534ece3b7
Ahmad Assaf <ahmadassaf@paluniv.edu.ps>
2021-11-08 09:58:43 +0200

    sql

---
+++ Examples/chapter4/db1.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 08, 2021 at 12:55 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `myguests`
+--
+
+CREATE TABLE `myguests` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `firstname` varchar(30) NOT NULL,
+  `lastname` varchar(30) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `reg_date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `myguests`
+--
+
+INSERT INTO `myguests` (`id`, `firstname`, `lastname`, `email`, `reg_date`) VALUES
+(1, 'John', 'Doe', 'john@example.com', '2021-11-07 22:58:41'),
+(2, 'Admam', 'Doe', 'john2@example.com', '2021-11-07 22:58:41'),
+(3, 'see', 'Doe', 'john@example.com', '2021-11-07 22:58:41'),
+(4, 'aaa', 'bbb', 'john@example.com', '2021-11-07 22:58:41');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `price` decimal(7,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `price`) VALUES
+(1, 'HP laptop-1', 'Fast HP laptop', '4500.00'),
+(2, 'Dell laptop-1', 'Fast Dell laptop', '4000.00'),
+(3, 'HP Printer 400', 'Laser HP Printer', '700.00'),
+(4, 'HP Printer 101', 'Laser HP Printer', '450.00'),
+(5, 'Mouse 40', 'Laser mouse', '80.00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `myguests`
+--
+ALTER TABLE `myguests`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `myguests`
+--
+ALTER TABLE `myguests`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
