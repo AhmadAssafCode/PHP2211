4ae46f37ffe0664bdf7dcd7aea863b4dd3bec753
Ahmad Assaf <ahmadassaf@paluniv.edu.ps>
2021-11-10 08:51:20 +0200

    update

--- Examples/chapter4/example4-1.php
+++ Examples/chapter4/example4-1.php
@@ -11,30 +11,32 @@
   try {
     // connect to MYSQL Database using PDO
     $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    
     // set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     //select query
     $sql = 'SELECT * FROM products';
-    $query = $pdo->query($sql);
+    $result = $pdo->query($sql);
 
-    // get all products as associative array
-    $products = $query->fetchAll(PDO::FETCH_ASSOC);
+    // get all product records as associative array
+    $products = $result->fetchAll(PDO::FETCH_ASSOC);
 
     if ($products) {
       echo '<h2>List of Products:</h2>';
       // show the products
-      foreach ($products as $products) {
-        echo $products['id'] . '<br>';
-        echo $products['name'] . '<br>';
-        echo $products['description'] . '<br>';
-        echo $products['price'] . '<br>';
+      foreach ($products as $product) {
+        echo $product['id'] . '<br>';
+        echo $product['name'] . '<br>';
+        echo $product['description'] . '<br>';
+        echo $product['price'] . '<br>';
+        echo '-----------------------------<br>';
       }
     }
   } catch (PDOException $e) {
     echo "Error: " . $e->getMessage();
   }
-    $pdo = null; //close PDO connection 
+  $pdo = null; //close PDO connection 
   ?>
 </body>
 
