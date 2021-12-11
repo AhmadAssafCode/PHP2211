853337894d0ee23beb1c3e2d3ec94b8b8582320c
Ahmad Assaf <ahmadassaf@paluniv.edu.ps>
2021-12-12 00:16:57 +0200

    ch6

---
+++ Examples/chapter6/example5-1/template/nav.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+      <li><a <?php? if(class=$_SERVER['PHP_SELF']"active" href="index.php">Home</a></li>
+      <li><a class="active" href="#">Paris</a></li>
+      <li><a class="active" href="contact_us.php">Contact Us</a></li>
+    </ul>
+  </nav>
+  
---
+++ Examples/chapter6/example6-3/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php $page_title="Home"?>
+
+<?php include("includes/head.php");?>
+
+<body>
+
+<?php include("includes/header.php");?>
+
+
+<main>
+
+
+  <article>
+    <h1>Home</h1>
+    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+  </article>
+<div class="container">
+  <div class="row">
+    <div class="col align-self-start">
+      One of three columns
+    </div>
+    <div class="col align-self-center">
+      One of three columns
+    </div>
+    <div class="col align-self-end">
+      One of three columns
+    </div>
+  </div>
+</div>
+</main>
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
+
