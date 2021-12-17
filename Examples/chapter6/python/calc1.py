443ddefcc13b1cea5289baabd123e014c6a87ff1
Ahmad Assaf <ahmadassaf@paluniv.edu.ps>
2021-12-18 01:37:17 +0200

    bs5

--- Examples/chapter6/python/calc1.py
+++
@@ -1,8 +0,0 @@
-a=0
-while(a!=-1):
-    a=int(input("ENTER A NUMBER: "))
-    #if a==-1:
-       # break
-    b=int(input("ENTER A NUMBER: "))
-    print(str(a)+"+"+str(b)+"="+str(a+b))
-print("GOOD BYE!")
--- Examples/chapter6/python/calc2.py
+++
@@ -1,8 +0,0 @@
-char='+'
-
-while(char!='e'):
-    a=int(input("ENTER FIRST NUMBER: "))
-    b=int(input("ENTER SECOND NUMBER: "))
-    print(str(a)+"+"+str(b)+"="+str(a+b))
-    char=input("ENTER + OR e TO END: ")
-print("GOOD BYE!")
